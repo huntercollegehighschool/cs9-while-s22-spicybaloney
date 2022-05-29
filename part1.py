64d7f30c1b78eafc17c917eb1399303b2b1b62bc
Frannie <104437002+spicybaloney@users.noreply.github.com>
2022-05-29 14:07:33 +0000

    Changed the first two, got confused near the last three

--- part1.py
+++ part1.py
@@ -11,10 +11,11 @@ Enter a number: 50
 Number of squares less than or equal to 50: 7
 
 '''
-num = int(input("Enter a number:"))
+num = int(input("enter a number: "))
 numbertosquare = 1
-numsquares = 0
-while numbertosquare ** 2 <= num:
+numsquares = 0 
+while numsquares ** 2 <= num:
     numsquares = numsquares + 1
-    
-print("Number of squares less than or equal to", str(num)+':', numsquares)
+
+print("number of squares less than or equal to", str(num)+':', numsquares)
+
